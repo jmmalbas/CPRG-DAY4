86b1ab521d2574796320abced748b0ab10061a1c
John Michael Malbas <923415@ACDM.DS.SAIT.CA>
2024-01-18 20:00:54 -0700

    Added some contents

---
+++ pages/employees.vue
@@ -0,0 +1,20 @@
+<script setup>
+// Fetch the data from a server route
+
+const { data } = await useFetch("/api/employees", {
+  headers: useRequestHeaders(["cookie"]),
+});
+const employees = data._value.employees;
+</script>
+
+<template>
+  <main>
+    <h1>Employees</h1>
+    <!-- render the fetched data -->
+    <ul>
+      <li v-for="employee in employees" :key="employee.EMPLOYEE_ID">
+        {{ employee.FIRST_NAME }} {{ employee.LAST_NAME }}
+      </li>
+    </ul>
+  </main>
+</template>
